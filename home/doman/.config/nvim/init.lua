cb48f08bdcd7b1cb8219850d7df70f529f4b1da0
zanadoman <domanseven@gmail.com>
2024-03-20 10:52:39 +0100

    .

---
+++ home/doman/.config/nvim/init.lua
@@ -0,0 +1,99 @@
+-- Line numbers
+
+vim.o.number = true
+
+-- Indentation width
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+-- Autosave
+
+vim.o.autowriteall = true
+
+-- Activate LSP on insert
+
+vim.diagnostic.config({
+	update_in_insert = true
+})
+
+-- Install plugins
+
+require('lazy').setup({
+    {
+		'm4xshen/autoclose.nvim'
+    },
+    {
+		'williamboman/mason.nvim',
+		'williamboman/mason-lspconfig.nvim',
+		'neovim/nvim-lspconfig'
+    },
+    {
+		'hrsh7th/nvim-cmp',
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer'
+    },
+    {
+		'L3MON4D3/LuaSnip'
+    },
+    {
+		'Exafunction/codeium.nvim',
+		'nvim-lua/plenary.nvim'
+    }
+})
+
+-- Start autoclose
+
+require('autoclose').setup()
+
+-- Start mason
+
+require('mason').setup()
+
+-- Start mason-lspconfig and install LSPs
+
+require('mason-lspconfig').setup({
+    ensure_installed = {
+		"clangd",
+		"csharp_ls"
+    }
+})
+
+-- Start and configure cmp
+
+require('cmp').setup({
+    snippet = {
+		expand = function(args)
+		require('luasnip').lsp_expand(args.body)
+    end},
+    sources = {
+		{name = 'nvim_lsp'},
+		{name = 'codeium'},
+		{name = 'buffer'}
+    },
+    mapping = {
+		['<Tab>'] = require('cmp').mapping.confirm({select = true}),
+		['<Escape>'] = require('cmp').mapping.abort(),
+		['<Up>'] = require('cmp').mapping.select_prev_item(select_opts),
+		['<Down>'] = require('cmp').mapping.select_next_item(select_opts)
+    },
+    window = {
+		completion = {
+	    	winhighlight = 'Normal:#00000000',
+	    	scrollbar = false
+		},
+		documentation = {
+	    	winhighlight = 'Normal:#00000000',
+	    	scrollbar = false
+		}
+    }
+})
+
+-- Start LSPs
+
+require('lspconfig').clangd.setup({})
+require('lspconfig').csharp_ls.setup({})
+
+-- Start codeium
+
+require('codeium').setup()
