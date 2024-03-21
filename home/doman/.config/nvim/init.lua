9aeaed4270db703945c3ae4f57c01d4f50962a71
zanadoman <domanseven@gmail.com>
2024-03-21 13:50:47 +0100

    .

--- home/doman/.config/nvim/init.lua
+++ home/doman/.config/nvim/init.lua
@@ -11,6 +11,10 @@ vim.o.shiftwidth = 4
 
 vim.o.colorcolumn = '80'
 
+-- Hook system clipboard
+
+vim.api.nvim_set_option('clipboard', 'unnamedplus')
+
 -- Activate LSP on insert
 
 vim.diagnostic.config({
@@ -20,34 +24,34 @@ vim.diagnostic.config({
 -- Install plugins
 
 require('lazy').setup({
-    {
-		'm4xshen/autoclose.nvim'
-    },
-    {
+	{
+		'windwp/nvim-autopairs'
+	},
+	{
 		'williamboman/mason.nvim',
 		'williamboman/mason-lspconfig.nvim',
-		'neovim/nvim-lspconfig'
-    },
-    {
+		'neovim/nvim-lspconfig',
+	},
+	{
 		'hrsh7th/nvim-cmp',
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/cmp-buffer'
-    },
-    {
+	},
+	{
 		'L3MON4D3/LuaSnip'
-    },
-    {
+	},
+	{
 		'Exafunction/codeium.nvim',
 		'nvim-lua/plenary.nvim'
-    },
+	},
 	{
 		'Mofiqul/vscode.nvim'
 	}
 })
 
--- Start autoclose
+-- Start nvim-autopairs
 
-require('autoclose').setup()
+require('nvim-autopairs').setup()
 
 -- Start mason
 
@@ -56,13 +60,13 @@ require('mason').setup()
 -- Start mason-lspconfig and install LSPs
 
 require('mason-lspconfig').setup({
-    ensure_installed = {
+	ensure_installed = {
 		'clangd',
 		'csharp_ls',
 		'jdtls',
 		'pyright',
 		'intelephense'
-    }
+	}
 })
 
 -- Start cmp and configure luasnip
@@ -78,8 +82,8 @@ require('cmp').setup({
 		{name = 'buffer'}
     },
     mapping = {
-		['<Up>'] = require('cmp').mapping.select_prev_item(select_opts),
-		['<Down>'] = require('cmp').mapping.select_next_item(select_opts),
+		['<Up>'] = require('cmp').mapping.select_prev_item(),
+		['<Down>'] = require('cmp').mapping.select_next_item(),
 		['<Tab>'] = require('cmp').mapping.confirm({select = true}),
 		['<Escape>'] = require('cmp').mapping.abort(),
 		['<C-Up>'] = require('cmp').mapping.scroll_docs(-1),
@@ -95,26 +99,40 @@ require('cmp').setup({
 
 vim.o.updatetime = 1000
 
-vim.api.nvim_create_autocmd('CursorHold', {
-    callback = function()
-		vim.lsp.buf.hover()
+vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
+	callback = function()
+		if not require('cmp').visible() then
+			vim.lsp.buf.hover()
+		end
 	end
 })
 
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-    vim.lsp.handlers.hover,
-    {
-        border = 'rounded'
-    }
+	vim.lsp.handlers.hover, {
+		border = 'rounded'
+	}
 )
+
 
 -- Start LSPs
 
-require('lspconfig').clangd.setup({})
-require('lspconfig').csharp_ls.setup({})
-require('lspconfig').jdtls.setup({})
-require('lspconfig').pyright.setup({})
-require('lspconfig').intelephense.setup({})
+require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig').clangd.setup({
+	capabilities = require('cmp_nvim_lsp').default_capabilities()
+})
+require('lspconfig').csharp_ls.setup({
+	capabilities = require('cmp_nvim_lsp').default_capabilities()
+})
+require('lspconfig').jdtls.setup({
+	capabilities = require('cmp_nvim_lsp').default_capabilities()
+})
+require('lspconfig').pyright.setup({
+	capabilities = require('cmp_nvim_lsp').default_capabilities()
+})
+require('lspconfig').intelephense.setup({
+	capabilities = require('cmp_nvim_lsp').default_capabilities()
+})
 
 -- Start codeium
 
