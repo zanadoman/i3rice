c88c8292259ddfaff35454a2feb44548b0c2a419
zanadoman <domanseven@gmail.com>
2024-03-22 16:21:27 +0100

    .

--- home/doman/.config/nvim/init.lua
+++ home/doman/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- Line numbers
 
 vim.o.number = true
+vim.o.relativenumber = true
 
 -- Indentation width
 
@@ -19,11 +20,19 @@ vim.o.clipboard = 'unnamedplus'
 
 vim.o.updatetime = 1000
 
+-- Change directory to file location
+
+vim.o.autochdir = true
+
 -- Install plugins
 
 require('lazy').setup({
 	{
 		'windwp/nvim-autopairs',
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		'nvim-telescope/telescope-file-browser.nvim'
 	},
 	{
 		'lewis6991/gitsigns.nvim'
@@ -34,12 +43,19 @@ require('lazy').setup({
 		'neovim/nvim-lspconfig',
 	},
 	{
-		'hrsh7th/nvim-cmp',
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer'
+		'L3MON4D3/LuaSnip'
 	},
 	{
-		'L3MON4D3/LuaSnip'
+		'hrsh7th/nvim-cmp',
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-cmdline',
+		'hrsh7th/cmp-path'
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		'romgrk/barbar.nvim',
+		'nvim-tree/nvim-web-devicons'
 	},
 	{
 		'Exafunction/codeium.nvim',
@@ -53,13 +69,15 @@ require('lazy').setup({
 -- Load plugins
 
 local autopairs = require('nvim-autopairs')
+local telescope = require('telescope')
 local gitsigns = require('gitsigns')
 local mason = require('mason')
 local mason_lsp = require('mason-lspconfig')
 local lspconfig = require('lspconfig')
+local luasnip = require('luasnip')
 local cmp = require('cmp')
 local cmp_lsp = require('cmp_nvim_lsp')
-local luasnip = require('luasnip')
+local lualine = require('lualine')
 local codeium = require('codeium')
 local vscode = require('vscode')
 
@@ -67,19 +85,25 @@ local vscode = require('vscode')
 
 autopairs.setup()
 
+-- Telescope
+
+telescope.load_extension('file_browser')
+telescope.load_extension('project')
+
 -- Git integration
 
 gitsigns.setup({
 	on_attach = function()
 		vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
         	callback = function()
-            	gitsigns.preview_hunk()
+				if not cmp.visible() then
+            		gitsigns.preview_hunk()
+				end
        		end
 		})
 	end,
-
 	preview_config = {
-		border = 'rounded',
+		border = 'rounded'
 	}
 })
 
@@ -107,29 +131,50 @@ vim.diagnostic.config({
 
 -- Setup autocompletion
 
+vim.o.pumheight = 10
+
 cmp.setup({
     snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body)
-    	end
+		end
 	},
     sources = {
 		{name = 'nvim_lsp'},
 		{name = 'codeium'},
-		{name = 'buffer'}
+		{name = 'buffer'},
+		{name = 'path'}
     },
     mapping = {
 		['<Up>'] = cmp.mapping.select_prev_item(),
 		['<Down>'] = cmp.mapping.select_next_item(),
 		['<Tab>'] = cmp.mapping.confirm({select = true}),
 		['<Escape>'] = cmp.mapping.abort(),
-		['<C-Up>'] = cmp.mapping.scroll_docs(-1),
+		['<C-Up>'] = cmp.mapping.scroll_docs(-1),	
 		['<C-Down>'] = cmp.mapping.scroll_docs(1)
-    },
+	},
     window = {
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered()
     }
+})
+
+cmp.setup.cmdline(':', {
+	sources = {
+		{name = 'cmdline'}
+	},
+})
+
+cmp.setup.cmdline('/', {
+	sources = {
+		{name = 'buffer'}
+	},
+})
+
+cmp.setup.cmdline('!', {
+	sources = {
+		{name = 'path'}
+	},
 })
 
 -- Enable info popup
@@ -143,8 +188,9 @@ vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
 })
 
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-	vim.lsp.handlers.hover,
-	{border = 'rounded'}
+	vim.lsp.handlers.hover, {
+		border = 'rounded'
+	}
 )
 
 -- Start servers
@@ -156,6 +202,12 @@ for _, server in ipairs(servers) do
 		capabilities = capabilities
 	})
 end
+
+-- Status line
+
+vim.o.showmode = false
+
+lualine.setup({})
 
 -- Start codeium
 
