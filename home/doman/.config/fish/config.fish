56e413c9ead7dac0c118c88f5542613b0b6eba63
zanadoman <domanseven@gmail.com>
2024-04-02 10:12:30 +0200

    .

--- home/doman/.config/fish/config.fish
+++ home/doman/.config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     export PATH="/home/doman/.path:$PATH"
     alias clear="clear && fastfetch"
-    alias startx="cd && startx && clear"
+    alias startx="startx && clear"
     clear
     starship init fish | source
 end
