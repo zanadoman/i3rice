cedfe34f0985b7df1b30a4e4458493f45350c077
zanadoman <domanseven@gmail.com>
2024-08-27 22:52:36 +0200

    .

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,3 +1,30 @@
+-- Neovim options
+vim.g.mapleader = ' '
+vim.o.autochdir = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.colorcolumn = '80'
+vim.o.cursorline = true
+vim.o.expandtab = true
+vim.o.ignorecase = true
+vim.o.mouse = '';
+vim.o.number = true
+vim.o.pumheight = 10
+vim.o.relativenumber = true
+vim.o.shiftwidth = 4
+vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+vim.o.smartcase = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.tabstop = 4
+vim.o.updatetime = 1000
+vim.o.wrap = false
+
+-- Neovim keymaps
+vim.keymap.set('v', '<c-up>', ':m \'<-2\ngv=gv')
+vim.keymap.set('v', '<c-down>', ':m \'>+1\ngv=gv')
+vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
+
 -- Language servers
 local servers = {
     'bashls',
@@ -21,64 +48,7 @@ local servers_opts = {
     lua_ls = { settings = { Lua = { diagnostics = { globals = { 'vim' } } } } }
 }
 
--- Leader
-vim.g.mapleader = ' '
-
--- Line numbers
-vim.o.number = true
-vim.o.relativenumber = true
-
--- Indentation
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-
--- Cursorline
-vim.o.cursorline = true
-
--- Columns
-vim.o.signcolumn = 'yes'
-vim.o.colorcolumn = '80'
-
--- System clipboard
-vim.o.clipboard = 'unnamedplus'
-
--- Autochange directory
-vim.o.autochdir = true
-
--- Wrap lines
-vim.o.wrap = false
-
--- Search mode
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Split
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Exit terminal
-vim.keymap.set('t', '<esc>', '<C-\\><C-n>')
-
--- Update time
-vim.o.updatetime = 1000
-
--- Disable mouse
-vim.o.mouse = '';
-
--- Move lines
-vim.keymap.set('v', '<C-down>', ':m \'>+1\ngv=gv')
-vim.keymap.set('v', '<C-up>', ':m \'<-2\ngv=gv')
-
--- Auto highlight
-vim.api.nvim_create_autocmd('InsertEnter', {
-    callback = function()
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(
-            '<CMD>nohlsearch\n', true, false, true), 'n', false)
-    end
-})
-
--- Fix files
+-- File formatter
 vim.api.nvim_create_autocmd('BufReadPost', {
     callback = function(opts)
         vim.cmd('silent! retab')
@@ -89,7 +59,15 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     end
 })
 
--- Plugin manager
+-- Auto highlight
+vim.api.nvim_create_autocmd('InsertEnter', {
+    callback = function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(
+            '<cmd>nohlsearch\n', true, false, true), 'n', false)
+    end
+})
+
+-- folke/lazy.nvim
 if vim.fn.isdirectory(vim.fn.stdpath('data') .. '/lazy/lazy.nvim') then
     vim.fn.system({
         'git',
@@ -102,41 +80,36 @@ end
 
 vim.opt.rtp:prepend(vim.fn.stdpath('data') .. '/lazy/lazy.nvim')
 
--- Install plugins
 require('lazy').setup(
     {
         {
+            'nvim-tree/nvim-web-devicons',
             'nvim-lua/plenary.nvim'
         },
         {
+            'folke/tokyonight.nvim',
             'nvimdev/dashboard-nvim',
             'nvim-lualine/lualine.nvim',
             'romgrk/barbar.nvim',
             'shortcuts/no-neck-pain.nvim',
-            'lukas-reineke/indent-blankline.nvim',
-            'folke/tokyonight.nvim',
-            'nvim-tree/nvim-web-devicons'
+            'arnamak/stay-centered.nvim',
+            'lukas-reineke/indent-blankline.nvim'
         },
         {
-            'arnamak/stay-centered.nvim',
             'windwp/nvim-autopairs',
             'kylechui/nvim-surround',
-            'numToStr/Comment.nvim',
+            'numToStr/Comment.nvim'
+        },
+        {
             'nvim-telescope/telescope.nvim',
             'nvim-telescope/telescope-file-browser.nvim',
-            'lewis6991/gitsigns.nvim',
-            'folke/which-key.nvim'
+            'lewis6991/gitsigns.nvim'
         },
         {
+            'neovim/nvim-lspconfig',
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
-            'neovim/nvim-lspconfig',
             'ray-x/lsp_signature.nvim'
-        },
-        {
-            'tpope/vim-dadbod',
-            'kristijanhusak/vim-dadbod-ui',
-            'kristijanhusak/vim-dadbod-completion'
         },
         {
             'L3MON4D3/LuaSnip',
@@ -145,6 +118,14 @@ require('lazy').setup(
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-path'
+        },
+        {
+            'tpope/vim-dadbod',
+            'kristijanhusak/vim-dadbod-ui',
+            'kristijanhusak/vim-dadbod-completion'
+        },
+        {
+            'folke/which-key.nvim'
         }
     },
     {
@@ -224,8 +205,6 @@ require('lualine').setup({
     }
 })
 
-vim.o.showmode = false
-
 -- Bufferline
 require('barbar').setup({
     animation = false,
@@ -266,8 +245,9 @@ vim.keymap.set('n', '<A-8>', ':BufferGoto 8\n', { silent = true })
 vim.keymap.set('n', '<A-9>', ':BufferGoto 9\n', { silent = true })
 vim.keymap.set('n', '<A-0>', ':BufferGoto 10\n', { silent = true })
 
--- Center buffer
+-- shortcuts/no-neck-pain.nvim, arnamak/stay-centered.nvim
 require('no-neck-pain').setup({ autocmds = { enableOnVimEnter = true } })
+require('stay-centered').setup({ skip_filetypes = { 'dashboard' } })
 vim.keymap.set('n', '<leader>z', ':NoNeckPain\n', {
     silent = true,
     desc = '󰘞 Zen mode'
@@ -280,75 +260,69 @@ require('ibl').setup({
     exclude = { filetypes = { 'dashboard' } }
 })
 
--- Theme
+-- tokyonight.nvim
 require('tokyonight').setup({ style = 'night', transparent = true })
 vim.cmd('colorscheme tokyonight')
-vim.cmd('highlight BufferScrollArrow guibg=#16161e guifg=#0cb0cc')
-vim.cmd('highlight BufferTabpageFill guibg=#16161e guifg=#15161e')
-vim.cmd('highlight BufferAlternate guibg=#16161e guifg=#565f89')
-vim.cmd('highlight BufferAlternateADDED guibg=#16161e guifg=#449dab')
-vim.cmd('highlight BufferAlternateCHANGED guibg=#16161e guifg=#6183bb')
-vim.cmd('highlight BufferAlternateDELETED guibg=#16161e guifg=#914c54')
-vim.cmd('highlight BufferAlternateERROR guibg=#16161e guifg=#db4b4b')
-vim.cmd('highlight BufferAlternateHINT guibg=#16161e guifg=#1abc9c')
-vim.cmd('highlight BufferAlternateIndex guibg=#16161e guifg=#0cb0cc')
-vim.cmd('highlight BufferAlternateINFO guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferAlternateMod guibg=#16161e guifg=#e0af68')
-vim.cmd('highlight BufferAlternateSign guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferAlternateSignRight guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferAlternateWARN guibg=#16161e guifg=#e0af68')
-vim.cmd('highlight BufferCurrent guibg=#16161e guifg=#c0caf5')
-vim.cmd('highlight BufferCurrentADDED guibg=#16161e guifg=#449dab')
-vim.cmd('highlight BufferCurrentCHANGED guibg=#16161e guifg=#6183bb')
-vim.cmd('highlight BufferCurrentDELETED guibg=#16161e guifg=#914c54')
-vim.cmd('highlight BufferCurrentERROR guibg=#16161e guifg=#db4b4b')
-vim.cmd('highlight BufferCurrentHINT guibg=#16161e guifg=#1abc9c')
-vim.cmd('highlight BufferCurrentIndex guibg=#16161e guifg=#0cb0cc')
-vim.cmd('highlight BufferCurrentINFO guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferCurrentMod guibg=#16161e guifg=#e0af68')
-vim.cmd('highlight BufferCurrentSign guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferCurrentSignRight guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferCurrentWARN guibg=#16161e guifg=#e0af68')
-vim.cmd('highlight BufferInactive guibg=#16161e guifg=#565f89')
-vim.cmd('highlight BufferInactiveADDED guibg=#16161e guifg=#449dab')
-vim.cmd('highlight BufferInactiveCHANGED guibg=#16161e guifg=#6183bb')
-vim.cmd('highlight BufferInactiveDELETED guibg=#16161e guifg=#914c54')
-vim.cmd('highlight BufferInactiveERROR guibg=#16161e guifg=#db4b4b')
-vim.cmd('highlight BufferInactiveHINT guibg=#16161e guifg=#1abc9c')
-vim.cmd('highlight BufferInactiveIndex guibg=#16161e guifg=#0cb0cc')
-vim.cmd('highlight BufferInactiveINFO guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferInactiveMod guibg=#16161e guifg=#e0af68')
-vim.cmd('highlight BufferInactiveSign guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferInactiveSignRight guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferInactiveWARN guibg=#16161e guifg=#e0af68')
-vim.cmd('highlight BufferVisible guibg=#16161e guifg=#c0caf5')
-vim.cmd('highlight BufferVisibleADDED guibg=#16161e guifg=#449dab')
-vim.cmd('highlight BufferVisibleCHANGED guibg=#16161e guifg=#6183bb')
-vim.cmd('highlight BufferVisibleDELETED guibg=#16161e guifg=#914c54')
-vim.cmd('highlight BufferVisibleERROR guibg=#16161e guifg=#db4b4b')
-vim.cmd('highlight BufferVisibleHINT guibg=#16161e guifg=#1abc9c')
-vim.cmd('highlight BufferVisibleIndex guibg=#16161e guifg=#0cb0cc')
-vim.cmd('highlight BufferVisibleINFO guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferVisibleMod guibg=#16161e guifg=#e0af68')
-vim.cmd('highlight BufferVisibleSign guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferVisibleSignRight guibg=#16161e guifg=#0db9d7')
-vim.cmd('highlight BufferVisibleWARN guibg=#16161e guifg=#e0af68')
+vim.api.nvim_set_hl(0, 'BufferScrollArrow', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferTabpageFill', { bg = '#16161e', fg = '#16161e' })
+vim.api.nvim_set_hl(0, 'BufferAlternate', { bg = '#16161e', fg = '#565f89' })
+vim.api.nvim_set_hl(0, 'BufferAlternateADDED', { bg = '#16161e', fg = '#449dab' })
+vim.api.nvim_set_hl(0, 'BufferAlternateCHANGED', { bg = '#16161e', fg = '#6183bb' })
+vim.api.nvim_set_hl(0, 'BufferAlternateDELETED', { bg = '#16161e', fg = '#914c54' })
+vim.api.nvim_set_hl(0, 'BufferAlternateERROR', { bg = '#16161e', fg = '#db4b4b' })
+vim.api.nvim_set_hl(0, 'BufferAlternateHINT', { bg = '#16161e', fg = '#1abc9c' })
+vim.api.nvim_set_hl(0, 'BufferAlternateIndex', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferAlternateINFO', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferAlternateMod', { bg = '#16161e', fg = '#e0af68' })
+vim.api.nvim_set_hl(0, 'BufferAlternateSign', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferAlternateSignRight', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferAlternateWARN', { bg = '#16161e', fg = '#e0af68' })
+vim.api.nvim_set_hl(0, 'BufferCurrent', { bg = '#16161e', fg = '#c0caf5' })
+vim.api.nvim_set_hl(0, 'BufferCurrentADDED', { bg = '#16161e', fg = '#449dab' })
+vim.api.nvim_set_hl(0, 'BufferCurrentCHANGED', { bg = '#16161e', fg = '#6183bb' })
+vim.api.nvim_set_hl(0, 'BufferCurrentDELETED', { bg = '#16161e', fg = '#914c54' })
+vim.api.nvim_set_hl(0, 'BufferCurrentERROR', { bg = '#16161e', fg = '#db4b4b' })
+vim.api.nvim_set_hl(0, 'BufferCurrentHINT', { bg = '#16161e', fg = '#1abc9c' })
+vim.api.nvim_set_hl(0, 'BufferCurrentIndex', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferCurrentINFO', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferCurrentMod', { bg = '#16161e', fg = '#e0af68' })
+vim.api.nvim_set_hl(0, 'BufferCurrentSign', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferCurrentSignRight', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferCurrentWARN', { bg = '#16161e', fg = '#e0af68' })
+vim.api.nvim_set_hl(0, 'BufferInactive', { bg = '#16161e', fg = '#565f89' })
+vim.api.nvim_set_hl(0, 'BufferInactiveADDED', { bg = '#16161e', fg = '#449dab' })
+vim.api.nvim_set_hl(0, 'BufferInactiveCHANGED', { bg = '#16161e', fg = '#6183bb' })
+vim.api.nvim_set_hl(0, 'BufferInactiveDELETED', { bg = '#16161e', fg = '#914c54' })
+vim.api.nvim_set_hl(0, 'BufferInactiveERROR', { bg = '#16161e', fg = '#db4b4b' })
+vim.api.nvim_set_hl(0, 'BufferInactiveHINT', { bg = '#16161e', fg = '#1abc9c' })
+vim.api.nvim_set_hl(0, 'BufferInactiveIndex', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferInactiveINFO', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferInactiveMod', { bg = '#16161e', fg = '#e0af68' })
+vim.api.nvim_set_hl(0, 'BufferInactiveSign', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferInactiveSignRight', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferInactiveWARN', { bg = '#16161e', fg = '#e0af68' })
+vim.api.nvim_set_hl(0, 'BufferVisible', { bg = '#16161e', fg = '#c0caf5' })
+vim.api.nvim_set_hl(0, 'BufferVisibleADDED', { bg = '#16161e', fg = '#449dab' })
+vim.api.nvim_set_hl(0, 'BufferVisibleCHANGED', { bg = '#16161e', fg = '#6183bb' })
+vim.api.nvim_set_hl(0, 'BufferVisibleDELETED', { bg = '#16161e', fg = '#914c54' })
+vim.api.nvim_set_hl(0, 'BufferVisibleERROR', { bg = '#16161e', fg = '#db4b4b' })
+vim.api.nvim_set_hl(0, 'BufferVisibleHINT', { bg = '#16161e', fg = '#1abc9c' })
+vim.api.nvim_set_hl(0, 'BufferVisibleIndex', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferVisibleINFO', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferVisibleMod', { bg = '#16161e', fg = '#e0af68' })
+vim.api.nvim_set_hl(0, 'BufferVisibleSign', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferVisibleSignRight', { bg = '#16161e', fg = '#0db9d7' })
+vim.api.nvim_set_hl(0, 'BufferVisibleWARN', { bg = '#16161e', fg = '#e0af68' })
 
--- Center cursorline
-require('stay-centered').setup({ skip_filetypes = { 'dashboard' } })
-
--- Bracket autocompletion
+-- windwp/nvim-autopairs, kylechui/nvim-surround, numToStr/Comment.nvim
 require('nvim-autopairs').setup()
-
--- Surround tool
 require('nvim-surround').setup()
-
--- Bulk commenter
 require('Comment').setup()
 require('Comment.ft').set('plsql', '--%s')
 
---Fuzzy finder
+-- nvim-telescope/telescope.nvim, nvim-telescope/telescope-file-browser.nvim
 require('telescope').setup()
+require('telescope').load_extension('file_browser')
 vim.keymap.set('n', '<leader>f', ':Telescope\n', {
     silent = true,
     desc = '󰭎 Telescope'
@@ -404,16 +378,8 @@ require('gitsigns').setup({
     end
 })
 
--- Key bindings
-require('which-key').setup({
-    win = { border = 'rounded' },
-    icons = {
-        group = '',
-        mappings = false
-    }
-})
-
--- Install servers
+-- neovim/nvim-lspconfig, williamboman/mason.nvim, williamboman/mason-lspconfig.nvim
+require('lspconfig.ui.windows').default_options.border = 'rounded'
 require('mason').setup({ ui = { border = 'rounded' } })
 require('mason-lspconfig').setup({ ensure_installed = servers })
 
@@ -421,28 +387,32 @@ vim.keymap.set('n', '<leader>l', ':LspInfo\n', {
     silent = true,
     desc = ' LSP'
 })
-vim.keymap.set('n', '<leader>ln', vim.lsp.buf.rename, {
-    silent = true,
-    desc = ' Rename'
-})
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, {
     silent = true,
     desc = ' Format'
 })
-vim.keymap.set('n', '<leader>ld', require('telescope.builtin').lsp_definitions, {
+vim.keymap.set('n', '<leader>ln', vim.lsp.buf.rename, {
     silent = true,
-    desc = ' Jump to definition'
-})
-vim.keymap.set('n', '<leader>lr', require('telescope.builtin').lsp_references, {
-    silent = true,
-    desc = ' Jump to references'
+    desc = ' Rename'
 })
 vim.keymap.set('n', '<leader>ll', require('telescope.builtin').diagnostics, {
     silent = true,
-    desc = ' List diagnostics'
+    desc = ' Diagnostics'
+})
+vim.keymap.set('n', '<leader>ld', require('telescope.builtin').lsp_definitions, {
+    silent = true,
+    desc = ' Definition'
+})
+vim.keymap.set('n', '<leader>lr', require('telescope.builtin').lsp_references, {
+    silent = true,
+    desc = ' References'
 })
 
-require('lspconfig.ui.windows').default_options.border = 'rounded'
+for _, server in ipairs(servers) do
+    local opts = servers_opts[server] or {}
+    opts.capabilities = require('cmp_nvim_lsp').default_capabilities()
+    require('lspconfig')[server].setup(opts)
+end
 
 -- Diagnostic config
 vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
@@ -483,12 +453,12 @@ require('cmp').setup({
         documentation = require('cmp').config.window.bordered()
     },
     mapping = {
-        ['<C-f>'] = require('cmp').mapping.confirm({ select = true }),
-        ['<C-e>'] = require('cmp').mapping.abort(),
-        ['<C-Up>'] = require('cmp').mapping.select_prev_item(),
-        ['<C-Down>'] = require('cmp').mapping.select_next_item(),
-        ['<A-Down>'] = require('cmp').mapping.scroll_docs(1),
-        ['<A-Up>'] = require('cmp').mapping.scroll_docs(-1)
+        ['<c-f>'] = require('cmp').mapping.confirm({ select = true }),
+        ['<c-e>'] = require('cmp').mapping.abort(),
+        ['<c-up>'] = require('cmp').mapping.select_prev_item(),
+        ['<c-down>'] = require('cmp').mapping.select_next_item(),
+        ['<a-down>'] = require('cmp').mapping.scroll_docs(1),
+        ['<a-up>'] = require('cmp').mapping.scroll_docs(-1)
     },
     sources = {
         { name = 'nvim_lsp' },
@@ -502,11 +472,12 @@ require('cmp').setup({
 
 require('cmp').setup.cmdline(':', { sources = { { name = 'cmdline' } } })
 require('cmp').setup.cmdline('/', { sources = { { name = 'buffer' } } })
-vim.o.pumheight = 10
 
--- Start language servers
-for _, server in ipairs(servers) do
-    local opts = servers_opts[server] or {}
-    opts.capabilities = require('cmp_nvim_lsp').default_capabilities()
-    require('lspconfig')[server].setup(opts)
-end
+-- folke/which-key.nvim
+require('which-key').setup({
+    win = { border = 'rounded' },
+    icons = {
+        group = '',
+        mappings = false
+    }
+})
